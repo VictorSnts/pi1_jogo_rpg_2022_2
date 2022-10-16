63e19aeb925f697171e5e0a723aa3c8e16db9ccd
Victor Santos <vfreitass16@icloud.com>
2022-10-16 18:50:37 -0300

    Incluindo TODOs e desenvolvendo a logica da função para definir as perguntas e alternativas

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+// TODO Criar funcao que recebe a lista de alternativas das perguntas e embaralha
+
 // Equacao de exemplo: x² – 5x + 6 = 0
 
 public class Jogo {
@@ -7,6 +9,7 @@ public class Jogo {
     static Scanner entrada = new Scanner(System.in);
     static String personagem;
 
+    // TODO Criar um texto de bem vindo para o jogo e incluir na função
     static void bemvindo() {
         // Imprime menssagem de boas vindas
         System.out.println("<Saudacoes de bem vindo aqui>");
@@ -50,18 +53,20 @@ public class Jogo {
 
     static void define_nome_personagem() {
         // Pede e define o nome do personagem
-        System.out.println("");
+        System.out.println();
         System.out.println("Durante o jogo, você interpretará o papel de ajudante do pirata Barba Verde. Defina o nome" +
                 "do seu personagem: ");
         personagem = entrada.next();
     }
 
+    // TODO Incluir a descrição da historia na funcao
     static void historia() {
         // Imprime a historia
         System.out.println("\n\n");
         System.out.println("<Descricao da historia aqui>");
     }
 
+    // TODO Revisar as instrucoes do jogo
     static void instrucoes() {
         // Imprime as instruções do jogo
         System.out.println("\n\n");
@@ -102,10 +107,68 @@ public class Jogo {
         }
     }
 
+    // TODO Incluir as perguntas reais na função
+    static void get_pergunta(Integer pergunta){
+        // Define e retorna um dicionario com os valores de Pergunta:[Lista de alteranativas].
+        // Neste retorno, por padrao a primeira alternativa será a correta.
+        Dictionary<Integer,ArrayList<String>> perguntas = new Hashtable<>();
+
+        // PERGUNTA 1
+        ArrayList<String> pergunta1 = new ArrayList<>();
+        pergunta1.add("<PERGUNTA 1>"); // PERGUNTA
+        pergunta1.add("Correta"); // ALT CORRETA
+        pergunta1.add("Incorreta");
+        pergunta1.add("Incorreta");
+        pergunta1.add("Incorreta");
+        pergunta1.add("Incorreta");
+
+        // PERGUNTA 2
+        ArrayList<String> pergunta2 = new ArrayList<>();
+        pergunta2.add("<PERGUNTA 2>"); // PERGUNTA
+        pergunta2.add("Correta"); // ALT CORRETA
+        pergunta2.add("Incorreta");
+        pergunta2.add("Incorreta");
+        pergunta2.add("Incorreta");
+        pergunta2.add("Incorreta");
+
+        // PERGUNTA 3
+        ArrayList<String> pergunta3 = new ArrayList<>();
+        pergunta3.add("<PERGUNTA 3>"); // PERGUNTA
+        pergunta3.add("Correta"); // ALT CORRETA
+        pergunta3.add("Incorreta");
+        pergunta3.add("Incorreta");
+        pergunta3.add("Incorreta");
+        pergunta3.add("Incorreta");
+
+        // PERGUNTA 4
+        ArrayList<String> pergunta4 = new ArrayList<>();
+        pergunta4.add("<PERGUNTA 4>"); // PERGUNTA
+        pergunta4.add("Correta"); // ALT CORRETA
+        pergunta4.add("Incorreta");
+        pergunta4.add("Incorreta");
+        pergunta4.add("Incorreta");
+        pergunta4.add("Incorreta");
+
+        perguntas.put(1, pergunta1);
+        perguntas.put(2, pergunta2);
+        perguntas.put(3, pergunta3);
+        perguntas.put(4, pergunta4);
+
+        switch (pergunta){
+            case 1 -> System.out.println(perguntas.get(1));
+            case 2 -> System.out.println(perguntas.get(2));
+            case 3 -> System.out.println(perguntas.get(3));
+            case 4 -> System.out.println(perguntas.get(4));
+            default -> System.out.println("Pergunta nao existe.");
+        }
+
+    }
+
     static void jogar() {
         // Funcao principal do jogo
         historia();
         define_nome_personagem();
+        get_pergunta(entrada.nextInt());
     }
 
     public static void main(String[] args) {
@@ -121,8 +184,7 @@ public class Jogo {
                         System.out.println("Chegamos ao final da partida. Gostaria de jogar novamente? (S ou N)");
                         continuar = entrada.next().toUpperCase();
                     } while (!valida("sair", continuar));
-                    if (continuar.equals("S")) break;
-                    else {
+                    if (continuar.equals("N")) {
                         creditos();
                         System.exit(0);
                     }
